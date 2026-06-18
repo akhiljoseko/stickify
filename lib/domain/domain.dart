094622c7ad8bbca559158c7d97b7f4b7705e8d3a
Akhil Jose <77378123+akhiljoseko@users.noreply.github.com>
2026-06-18 11:49:53 +0530

    feat: add infinite_scroll_pagination package and PaginatedResult entity

--- lib/domain/domain.dart
+++ lib/domain/domain.dart
@@ -18,6 +18,7 @@ export 'entities/feature_id.dart';
 export 'entities/ingredient.dart';
 export 'entities/label_template.dart';
 export 'entities/nutrition_facts.dart';
+export 'entities/paginated_result.dart';
 export 'entities/print_job.dart';
 export 'entities/printer_device.dart';
 export 'entities/product.dart';
